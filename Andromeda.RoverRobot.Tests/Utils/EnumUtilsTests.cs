bff62d022ae289526bbafb63e91d061c7906347e
Inam Gull <inam.gull@ansarada.com>
2020-08-29 21:46:46 +1000

    add unit tests for utils

---
+++ Andromeda.RoverRobot.Tests/Utils/EnumUtilsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Andromeda.RoverRobot.Utils;
+using Xunit;
+
+namespace Andromeda.RoverRobot.Tests.Utils
+{
+    public class EnumUtilsTests
+    {
+        [Theory]
+        [InlineData("North", Direction.North)]
+        [InlineData("SOUTH", Direction.South)]
+        [InlineData("EAST", Direction.East)]
+        [InlineData("WeST", Direction.West)]
+        public void GetEnumFromDescription_WhenEnumHasDescription_ShouldReturnEnum(string value, Direction direction)
+        {
+            //Act
+            var enumValue = EnumUtils.GetEnumFromDescription<Direction>(value);
+            
+            //Assert
+            Assert.Equal(direction, enumValue);
+        }
+        
+        [Fact]
+        public void GetEnumFromDescription_WhenEnumDoesntHaveDescription_ShouldThrowException()
+        {
+            //Act && ASSERT
+            Assert.Throws<ArgumentException>(() => EnumUtils.GetEnumFromDescription<Direction>("abctest"));
+        }
+    }
+}
--- Andromeda.RoverRobot/Utils/EnumUtils.cs
+++ Andromeda.RoverRobot/Utils/EnumUtils.cs
@@ -8,23 +8,27 @@ namespace Andromeda.RoverRobot.Utils
         public static T GetEnumFromDescription<T>(string description)
         {
             var type = typeof(T);
-            
-            foreach(var field in type.GetFields())
+
+            foreach (var field in type.GetFields())
             {
                 var attribute = Attribute.GetCustomAttribute(field,
                     typeof(DescriptionAttribute)) as DescriptionAttribute;
-                if(attribute != null)
-                {
-                    if(attribute.Description == description)
-                        return (T)field.GetValue(null);
-                }
-                else
-                {
-                    if(field.Name == description)
-                        return (T)field.GetValue(null);
-                }
+                if (attribute != null)
+
+                    if (description.Equals(attribute.Description, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        return (T) field.GetValue(null);
+                    }
+
+                    else
+                    {
+                        if (description.Equals(field.Name, StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            return (T) field.GetValue(null);
+                        }
+                    }
             }
-            
+
             throw new ArgumentException("Not found.", nameof(description));
         }
     }
